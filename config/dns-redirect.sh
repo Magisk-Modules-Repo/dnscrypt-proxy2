71869438b4bc7e016eb20be758c513e87f345335
lindroidux <51092143+lindroidux@users.noreply.github.com>
2020-02-09 15:25:40 +0530

    spelling corrections

--- config/dns-redirect.sh
+++ config/dns-redirect.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 
-IP6TABIP6TABLES=/system/bin/ip6tables
+IP6TABLES=/system/bin/ip6tables
 IPTABLES=/system/bin/iptables 
 
 $IPTABLES  -t nat -A OUTPUT -p tcp ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
