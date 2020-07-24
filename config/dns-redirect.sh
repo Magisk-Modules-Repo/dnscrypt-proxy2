bf320009f28e8c136b60838168a7704227a3e622
lindroidux <51092143+lindroidux@users.noreply.github.com>
2020-07-24 23:20:19 +0530

    UPDATED
    updated scripts from AFWALL custom scripts page

--- config/dns-redirect.sh
+++ config/dns-redirect.sh
@@ -3,5 +3,23 @@
 IP6TABLES=/system/bin/ip6tables
 IPTABLES=/system/bin/iptables 
 
-$IPTABLES  -t nat -A OUTPUT -p tcp ! -d 9.9.9.9 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
-$IPTABLES  -t nat -A OUTPUT -p udp ! -d 9.9.9.9 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
+# $IPTABLES  -t nat -A OUTPUT -p tcp  ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
+# $IPTABLES  -t nat -A OUTPUT -p udp ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
+
+# Disable ipv6
+# for FILE in `ls  /proc/sys/net/ipv6/conf/*/disable_ipv6`;do echo 1>$FILE;done;
+
+# Force a specific DNS
+# First two lines delete current DNS settings 
+$IPTABLES -t nat -D OUTPUT -p tcp --dport 53 -j DNAT --to-destination 127.0.0.1:5354 || true 
+$IPTABLES -t nat -D OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:5354 || true 
+
+# This two new lines set our new DNS running at 127.0.0.1 at port 5354 
+$IPTABLES -t nat -I OUTPUT -p tcp  ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
+$IPTABLES -t nat -I OUTPUT -p udp ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
+
+# For IPv6 we need to change it in a different way since there is no nat! 
+# $IP6TABLES -A INPUT -i $LAN_IF -s $LAN_NET -p udp –dport 53 -j ACCEPT 
+# $IP6TABLES -A FORWARD -i $LAN_IF -s $LAN_NET -p udp –dport 53 -j ACCEPT 
+# $IP6TABLES -A OUTPUT -p udp –dport 53 -j ACCEPT
+
